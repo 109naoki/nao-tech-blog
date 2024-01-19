2ac97b6d92e1068cf10fadf995a169bc31bbc573
argyle-ito <ito@argyle.jp>
2024-01-19 13:27:11 +0900

    ファイル修正

--- app/[categoryId]/page.tsx
+++
@@ -1,9 +0,0 @@
-import { useQuery } from "@tanstack/react-query";
-
-const fetchCategories = async (pathname: string) => {
-  const res = await fetch(`/api/category${pathname}`);
-  return res.json();
-};
-export default async function Page() {
-  return <h1>詳細ページ</h1>;
-}
